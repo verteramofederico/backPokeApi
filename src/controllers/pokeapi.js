3434fdd7d196369159f8b96891550c3edd4152ca
verteramofederico <verteramo.federico@gmail.com>
2021-11-20 14:56:03 -0300

    bug solved with api #

--- src/controllers/pokeapi.js
+++ src/controllers/pokeapi.js
@@ -7,19 +7,19 @@ const pokeapiController = {
     
     getAll: async (req, res) => {
         try {
-            fetch(`https://pokeapi.co/api/v2/pokemon/?limit=1119`)
+            fetch(`https://pokeapi.co/api/v2/pokemon/?limit=1118`)
             .then(response => response.json())
             .then(data => {datapoke = data
             res.status(200).json({datapoke})
             })        
         }catch (error) {
             console.log(error);
-            res.sendStatus(500);  
+            res.sendStatus(500) 
         }},
-        
+
     getById: async (req, res) => {
         let idParams = req.params.id
-        if (idParams !== null && idParams !== undefined) {
+        if (idParams !== null && idParams !== undefined && idParams <= 898) {
         try {
             fetch(`https://pokeapi.co/api/v2/pokemon/${idParams}`)
             .then(response => response.json())
@@ -29,10 +29,24 @@ const pokeapiController = {
             
         }catch (error) {
             console.log(error);
-            res.sendStatus(500);  
+            res.sendStatus(500) 
         }}
-        if (idParams === null || idParams === undefined) {
-            res.status(500).send({ error: 'You must enter a valid id' })
+        if (idParams !== null && idParams !== undefined && idParams > 898 && idParams <= 1118 ) {
+            try {
+                // hay un salto de 9103 en los id de Pokemon. En la siguiente linea se salva.
+                idToCall = parseInt(idParams) + 9102
+                fetch(`https://pokeapi.co/api/v2/pokemon/${idToCall}`)
+                .then(response => response.json())
+                .then(data => {datapoke = data
+                res.status(200).json({datapoke})
+                })
+                
+            }catch (error) {
+                console.log(error);
+                res.sendStatus(500) 
+            }}
+        if (idParams === null || idParams === undefined || idParams > 1118) {
+            res.status(500).send({ error: 'You must enter a valid id betwen 1 and 1118' })
         }
     }
 }
